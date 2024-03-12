7f1a0f790c2245ed7866f0aa628dc3c0c85cfe69
Mariabarbosa12 <160647209+Mariabarbosa12@users.noreply.github.com>
2024-03-12 15:24:42 -0300

    aula 12-3

---
+++ compenents/atividades_1/index.js
@@ -0,0 +1,14 @@
+import {View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Atividades_1 () {
+    return(
+       <View style={styles.container}>
+         <Text style={styles.texto} > Atividade 1</Text>
+         <Text style={styles.text} > Maria Julia</Text>    
+       </View>
+    );
+}
+
+export default Atividades_1;
---
+++ compenents/atividades_1/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet }  from 'react-native';
+
+const styles = StyleSheet.create ({
+
+   container:{
+    backgroundColor: 'purple',
+    padding: 10,
+   } ,
+
+   texto: {
+    color:'#00d',
+    fontSize: 20,
+   },
+
+   text: {
+    color:'#000',
+    fontSize: 20,
+   },
+
+
+});
+
+export default styles;
