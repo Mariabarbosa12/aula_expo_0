a371faa192ec9dab90145787aa7dba7b693a8435
Mariabarbosa12 <160647209+Mariabarbosa12@users.noreply.github.com>
2024-04-19 09:48:14 -0300

    aula 19-04

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ import Exemplo2 from './compenents/exemplo_2';
 
 import Atividades_1 from './compenents/atividades_1';
 
+import atividade_2 from './compenents/atividade_2';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo2/>
+      <atividade_2/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ compenents/atividade_2/index.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+
+import styles from './styles';
+
+function atividade_2 () {
+    return(
+     <View style={styles.container}>
+      <Text style={styles.text}>
+        Atividade 2
+      </Text>
+     </View>
+    );
+}
+
+export default atividade_2;
